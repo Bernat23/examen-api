0cba1d428e3e2336059094c08e3522da46bc2763
Bernat23 <bpujolriu771@boscdelacoma.cat>
2023-02-23 14:58:16 +0100

    api funciona

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const v1ProducteRouter = require("./v1/routes/productesRoutes");
 const v1EstocRouter = require("./v1/routes/estocsRoutes");
-const v1MaquinaRouter = require("./v1/routes/maquinesRoutes")
 
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('../swagger_output.json');
+//const swaggerDocument = require('../swagger_output.json');
 const cors = require("cors")
 
 
@@ -13,10 +12,9 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors())
-app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+//app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/api/v1/productes", v1ProducteRouter);
 app.use("/api/v1/estocs", v1EstocRouter);
-app.use("/api/v1/maquines", v1MaquinaRouter);
 
 
 app.listen(PORT, () => {
